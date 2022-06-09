a8a5cb667c5ab0e8f9ac7eed6301a96485026dc9
Manu <ellanos@linuxmail.org>
2022-06-08 20:56:24 -0500

    leaving original config

--- providers.tf
+++ providers.tf
@@ -7,8 +7,5 @@ terraform {
 }
 
 provider "aws" {
-  shared_config_files = ["/home/ellanos/.aws/config"]
-  shared_credentials_files = ["/home/ellanos/.aws/credentials"]
-  profile = "training"
   region = var.aws_region
 }
