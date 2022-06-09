d52601a4dfd6b810348ef1e114873f1a673bf4ad
Manu <ellanos@linuxmail.org>
2022-06-08 20:45:12 -0500

    Adding config file

--- providers.tf
+++ providers.tf
@@ -7,6 +7,8 @@ terraform {
 }
 
 provider "aws" {
+  shared_config_files = ["/home/ellanos/.aws/config"]
+  shared_credentials_files = ["/home/ellanos/.aws/credentials"]
+  profile = "training"
   region = var.aws_region
-  shared_credentials_file = ["/home/ellanos/.aws/credentials"]
 }
